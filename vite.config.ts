398a38d33ffbbf3f43a024a63b9a117b802d0090
Viscerall <nizaradisaputra12@gmail.com>
2026-01-14 15:50:24 +0700

    Fix base url matching repo name

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
 
-  base: "/nofo-landing-page", 
+  base: "/nofo-landing-page/", 
   
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
